de76141a9da1c4a7bc5e15e3fdf5b8f9e43bf3d2
Olivia-Lizbeth-Chairez-Alba <up240594@alumnos.upa.edu.mx>
2025-06-05 10:37:41 -0600

    comentario

---
+++ Day_1_level_2.py
@@ -0,0 +1,37 @@
+
+"""Ejercicios dia 1 nivel 2"""
+
+print("Hello , world");
+
+"""2.- Operadores"""
+print ("2 + 3");
+print(2 + 3);
+print (2 * 3);
+print (3 - 1);
+print (3 - 2);
+print (3 * 2);
+print (3 ** 2);
+print (2 ** 4);
+print (2 % 3);
+print (3 % 2);
+print (3 // 2);
+       
+#3.- "Palabras" de filas
+
+print("Olivia Lizbeth Chairez Alba");
+print("caca,chamoy,pepe,lolo");
+print("Mexico Ags");
+print("30 dia de python");
+
+"""4.- Tipos de datos:"""
+print((type(10)))  #entero o int
+print((type(3.14))); #decimal o float
+print((type(1 + 3j))); #complejo o complex
+print((type('Olivia'))); #char o str string
+print((type([1,2,3]))); #lista o list
+print((type({"Olivia":"popo"})));  #dictado o dict
+print((type ((2,3,4,5)))); #tuple
+print((type ({9.8,3.14,2.7}))); #set
+
+
+#la funcion salir es exit()
---
+++ Day_2_level_1.py
@@ -0,0 +1,29 @@
+
+"""Day 2: 30 days of python programming"""
+
+first_name="Olivia Lizbeth";
+last_name='Chairez Alba';
+full_name='Olivia Lizbeth Chairez Alba';
+country='Mexico';
+city='Aguascalientes';
+age=18;
+year=2025;
+is_married= False ;
+is_true=False;
+is_light_on= True;
+variable1,variable2,varibale3= 1,'cinco',True;
+
+"""Excercises level 2"""
+
+print(type(first_name));
+print(type(last_name));
+print(type(full_name));
+print(type(country));
+print(type(city));
+print(type(age));
+print(type(year));
+print(type(is_married));
+print(type(is_true));
+print(type(is_light_on));
+print(type(variable1,variable2,varibale3));
+
