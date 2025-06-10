a0fff7690ff8a32e89254e1a627f6a3286c7106e
UP240594 <up240594@alumnos.upa.edu.mx>
2025-06-10 00:15:09 -0600

    Termine el dia 2 y comence el dia 3 nivel 1

--- Day_2_level_1.py
+++ Day_2_level_1.py
@@ -13,22 +13,3 @@ is_true=False;
 is_light_on= True;
 x, y, z = True , 'oli' , 5
 
-"""Excercises level 2"""
-
-print(type(first_name));
-print(type(last_name));
-print(type(full_name));
-print(type(country));
-print(type(city));
-print(type(age));
-print(type(year));
-print(type(is_married));
-print(type(is_true));
-print(type(is_light_on));
-print(type(x) , type (y) , type (z));
-
-print(len(first_name));
-print(len(last_name));
-#parte 4 del dia 2
-nume_one=5;
-num_two=4;
---
+++ Day_2_level_2.py
@@ -0,0 +1,52 @@
+
+"""Excercises level 2"""
+
+print(type(first_name));
+print(type(last_name));
+print(type(full_name));
+print(type(country));
+print(type(city));
+print(type(age));
+print(type(year));
+print(type(is_married));
+print(type(is_true));
+print(type(is_light_on));
+print(type(x) , type (y) , type (z));
+
+print(len(first_name));
+print(len(last_name));
+#parte 4 del dia 2
+num_one=5;
+num_two=4;
+
+variable_Total=num_one+num_two;
+
+diff=num_two-num_one;
+
+product=num_two*num_one;
+
+division= num_one/num_two;
+
+remainder=num_two%num_one;
+
+exp=num_one**num_two;
+
+floor_division=num_one//num_two
+
+"""Radius of a circle"""
+radius=30;
+area_of_circle=3.1416*(radius**2);
+circum_of_circle=2*3.1416*radius;
+
+r=int(input("Give me the raidus of the circle please"))
+
+area_circle_user=3.1416*(r ** 2);
+
+print("The area is:", area_circle_user);
+
+first_names=input("Give the first name");
+last_names=input("Give your last name");
+countrys=input("Give your country");
+ages=input("The age please");
+
+help("keywords");
