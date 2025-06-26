4b6033c62b45ef55f500d82e3ccbaa12f0ff8022
UP240594 <up240594@alumnos.upa.edu.mx>
2025-06-25 23:57:41 -0600

    Dia 5 nivel 2 puntito 4

---
+++ day_05_level_02.py
@@ -0,0 +1,13 @@
+#1
+
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages .sort()
+print(ages)
+
+x=len(ages) / 2 #5
+position=ages[5]
+print(position , ages[6])
+
+operation=sum(ages) / len(ages)
+print(operation)
+
